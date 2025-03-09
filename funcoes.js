4b91380c0633cda214f098c7fb86c3d910c961c5
Gutoneitzke <gutoneitzke12345@gmail.com>
2025-03-09 16:53:08 -0300

    feat: functions lesson

---
+++ funcoes.js
@@ -0,0 +1,25 @@
+// function soma(numero1, numero2) {
+//     alert(numero1 + numero2)
+// }
+
+// soma(1, 8)
+
+// function saudacoes(nome) {
+//     return 'Olá! '+nome
+// }
+
+// console.log(saudacoes('Nanda'))
+
+// const multiplicacao = (x, y) => {
+//     return x * y
+// }
+
+// console.log(multiplicacao(2, 7))
+
+
+function escopo() {
+    var idade = 22
+    console.log(idade)
+}
+
+escopo()
